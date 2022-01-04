1701414ac026c6eb3cb8e6fb8769179b1864ddf0
Maciej Dzik <dzikdrums@gmail.com>
2022-01-04 09:23:13 +0100

    commit 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hehe</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>hehe</h1>
+            <h2>hehe3</h2>
+            <h3>hehe4</h3>
+        </div>
+    );
 }
 
 export default App;
